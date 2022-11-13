82e3cb3351dea2ac38d02180afc76434e7d8387f
G. Godwin <g.godwin@Gs-MacBook-Pro.local>
2022-11-13 13:28:50 -0500

    added hours to html and a neutral timeslot colour

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-$(function () {
+var hour = $('#hour-9')
+var thingsToDo = $('.description')
+
+$('.savebtn').click, function () {
+  localStorage.setItem('time', hour)
+  localStorage.setItem('toDo', thingsToDo)
+}
+
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
@@ -9,21 +16,32 @@ $(function () {
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
   //
+
+  $( "li" ).each(function( index ) {
+    console.log( index + ": " + $( this ).text() );
+  });
+
+
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
+
+
+  
   //
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
 
-  
+$(the place where it belongs).text?, function () {
+  localStorage.getItem('time', hour)
+  localStorage.getItem('toDo', thingsToDo)
+}
   //
   // TODO: Add code to display the current date in the header of the page.
   var now = dayjs();
   $('#currentDay').text(now.format('MMM D, YYYY'));
   console.log(now);
-  $('#currentDay').css("font-size", "50px")
-});
+  $('#currentDay').css("font-size", "50px");
