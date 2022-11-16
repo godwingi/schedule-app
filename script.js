aea3a467951576545c594ea589bd8be9da51e611
G. Godwin <g.godwin@Gs-MacBook-Pro.local>
2022-11-16 18:56:31 -0500

    upload changes

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-var hour = $('#hour-9')
-var thingsToDo = $('.description')
+// var hour = $('#hour-9')
+// var thingsToDo = $('.description')
 
-$('.savebtn').click, function () {
-  localStorage.setItem('time', hour)
-  localStorage.setItem('toDo', thingsToDo)
-}
+$('.savebtn').on('click', function () {
+  localStorage.setItem('time', this)
+  localStorage.setItem('toDo', this)
+})
 
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
@@ -17,9 +17,9 @@ $('.savebtn').click, function () {
   // useful when saving the description in local storage?
   //
 
-  $( "li" ).each(function( index ) {
-    console.log( index + ": " + $( this ).text() );
-  });
+  // $( "li" ).each(function( index ) {
+  //   console.log( index + ": " + $( this ).text() );
+  // });
 
 
   // TODO: Add code to apply the past, present, or future class to each time
@@ -29,19 +29,19 @@ $('.savebtn').click, function () {
   // current hour in 24-hour time?
 
 
-  
+
   //
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
 
-$(the place where it belongs).text?, function () {
-  localStorage.getItem('time', hour)
-  localStorage.getItem('toDo', thingsToDo)
-}
+$(document).ready(function () {
+  localStorage.getItem('time', this)
+  localStorage.getItem('toDo', this)
+})
   //
   // TODO: Add code to display the current date in the header of the page.
   var now = dayjs();
-  $('#currentDay').text(now.format('MMM D, YYYY'));
+  $('#currentDay').text(now.format('dddd, MMMM D, YYYY'));
   console.log(now);
   $('#currentDay').css("font-size", "50px");
